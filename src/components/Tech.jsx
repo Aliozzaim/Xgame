17e97d61566138fea64bfa8712d19b6f80898537
Ali Ozzaim <125793435+Aliozzaim@users.noreply.github.com>
2023-12-29 13:42:41 +0100

    add: cursor-pointer to the ballCancas

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -10,7 +10,9 @@ const Tech = () => {
     <div className="flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((technology) => (
         <div className="w-25 h-25" key={technology.name}>
-          <BallCanvas icon={technology.icon} />
+          <div className="cursor-pointer">
+            <BallCanvas icon={technology.icon} />
+          </div>
         </div>
       ))}
     </div>
